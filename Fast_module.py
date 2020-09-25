ab095cb569654e5cca6a65003222bd6d588b676d
lokeshwar <60348595+lokemass@users.noreply.github.com>
2020-09-25 08:05:30 +0530

    Create Fast_module.py

---
+++ Fast_module.py
@@ -0,0 +1,13 @@
+import math
+print("Fast modulo ")
+'''step by step aproch '''
+base= int(input('enter base value'))
+expo= int(input("enter exponent value"))
+mod= int(input("enter mod value"))
+i=1
+while(i<expo):
+	print(base,"^",i,"=",(base**i),"mod",mod)
+	print("=", ((base**i))%mod)
+	i=i*2
+print(base,"^",expo,"=",(base**expo,"mod",mod))
+print("=", ((base**expo))%mod)
