435eafd95cff633d55f11d003df26e7165e69337
lokeshwar <60348595+lokemass@users.noreply.github.com>
2020-09-28 11:07:30 +0530

    Update Fast_module.py

--- Fast_module.py
+++ Fast_module.py
@@ -5,16 +5,25 @@ import math
 
 ascii_banner = pyfiglet.figlet_format("Fast module ")
 print(ascii_banner )
-print("				-by lokemass")
+print("					-by lokemass")
 print("Fast modulo ")
 '''step by step aproch '''
 base= int(input('enter base value'))
 expo= int(input("enter exponent value"))
 mod= int(input("enter mod value"))
 i=1
-while(i<expo):
+temp = (base**i)%mod
+while(i<=expo):
 	print(base,"^",i,"=",(base**i),"mod",mod)
 	print("=", ((base**i))%mod)
+	print('or')
+	if (i >1):
+		temp1 = temp**2
+		print(base,"^",i,"=",temp1,"mod",mod)
+		print("=", temp1%mod)
+	
+	
 	i=i*2
 print(base,"^",expo,"=",(base**expo,"mod",mod))
 print("=", ((base**expo))%mod)
+ 
